0dda5c798a5d185d40a2c1f226790aa8d00505c0
yossyxp <yoshi.0406.smash@gmail.com>
2020-11-12 19:32:48 +0900

    入室イベント追加

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -2,6 +2,7 @@
 
 // 入室メッセージをサーバに送信する
 // 入力されたユーザ名を取得する
+<<<<<<< Updated upstream
 const userName = 'neko';
 // 入室メッセージイベントを送信する
 
@@ -9,3 +10,14 @@ const userName = 'neko';
 socket.on('', function (data) {
     $('#thread').prepend('<p>' + data+ '</p>');
 });
+=======
+const userName = $('#userName').val();
+// 入室メッセージイベントを送信する
+  const message = userName + 'さんが入室しました．';
+  socket.emit('sendEnterMessageEvent', message);
+
+// サーバから受信した入室メッセージを画面上に表示する
+socket.on('receiveEnterMessageEvent', function (data) {
+    $('#thread').prepend('<p>' + data +'</p>');
+});
+>>>>>>> Stashed changes
--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,7 +2,12 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
+<<<<<<< Updated upstream
     socket.on('send', function (data) {
         io.sockets.emit("send", data);
+=======
+    socket.on('sendEnterMessageEvent', function (data) {
+      socket.broadcast.emit('receiveEnterMessageEvent', data);
+>>>>>>> Stashed changes
     });
 };
