b17dbc4cd66e9f42ce6a16d51fb96e9f864bdad1
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2020-11-12 19:32:27 +0900

    追加機能1,3実装

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -7,14 +7,22 @@ function publish() {
     // 入力されたメッセージを取得
     const message = document.getElementById('message');
 
-    if (message.value === '') {
+    if (message.value.trim() === '') {
         return console.log('return起動');
     }
+
+    const createdAt = new Date();
+    const month = createdAt.getMonth() + 1;
+    const today = createdAt.getDate();
+    const hours = createdAt.getHours();
+    const minutes = createdAt.getMinutes();
+    const displayTime = (month + "/" + today + " " + hours + ":" + minutes);
 
     // 投稿内容を送信
     socket.emit('sendPostServer', {
         name: 'name',
-        text: message.value
+        text: message.value,
+        time: displayTime
     });
 
     //入力欄を空白にする
@@ -25,5 +33,5 @@ function publish() {
 
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('sendPostCliant', function (data) {
-    $('#thread').prepend('<p>' + data.name + ':' + data.text + '</p>');
+    $('#thread').prepend('<p>' + data.name + ' : ' + data.text + "　" + data.time + '</p>');
 });
