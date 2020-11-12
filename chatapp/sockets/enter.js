a923e0413ec69bfce928c8c353c1f975a1199a03
yossyxp <yoshi.0406.smash@gmail.com>
2020-11-12 19:43:16 +0900

    修正

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,12 +2,7 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-<<<<<<< Updated upstream
-    socket.on('send', function (data) {
-        io.sockets.emit("send", data);
-=======
     socket.on('sendEnterMessageEvent', function (data) {
       socket.broadcast.emit('receiveEnterMessageEvent', data);
->>>>>>> Stashed changes
     });
 };
