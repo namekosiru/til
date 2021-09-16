c02dc608e683f83205eb85884d005998d701c53b
namekosiru <rinjyou0031@toki.waseda.jp>
2021-09-16 15:53:44 +0900

    mediaの追加

--- django-sns/boardapp/views.py
+++ django-sns/boardapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login
-
+from .models import BoardModel
 # Create your views here.
 
 def signupfunc(request):
@@ -30,4 +30,6 @@ def loginfunc(request):
     return render(request, 'login.html', {'context': 'get method'})
 
 def listfunc(request):
-    return render(request, 'list.html')
+    object_list = BoardModel.objects.all()
+
+    return render(request, 'list.html', {'object_list':object_list})
