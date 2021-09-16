56237f933755186dbd4c06a9b7420495f9ae0572
namekosiru <rinjyou0031@toki.waseda.jp>
2021-09-16 15:12:45 +0900

    listviewの追加

--- django-sns/boardapp/urls.py
+++ django-sns/boardapp/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import signupfunc, loginfunc, listfunc
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('signup/', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
+    path('list/', listfunc, name='list')
 ]
